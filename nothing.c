3163e67394204bb7bc2c9564aeaf406d5a0641a9
Julian <63823807+Grimgrams@users.noreply.github.com>
2025-02-08 00:37:11 -0500

    fixed trash code

--- nothing.c
+++ nothing.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char **argv){
     for (int i=1; i<=argc; i++) {
-        nothing_char(argv[i]);
+        nothing(argv[i]);
     }
-    nothing(argc);
+    nothing(&argc); // dont say shit about this, assholes.
     return 0;
 }
